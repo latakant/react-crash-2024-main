a9b07af5a8c5e38d94288eb4c8f12e38352f52d0
syco <syco@kali>
2024-07-16 16:13:13 +0530

    Trip Started

---
+++ Trip_Planner/src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button"
+
+const App = () => {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+      <Button>Demo</Button>
+    </h1>
+  )
+}
+
+export default App
